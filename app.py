01263168f1a2a62d6185d2838f8c5dbf6e6b233f
dpdtydz <dpdtydz@donga.ac.kr>
2021-09-24 15:27:41 +0900

    add sign

--- app.py
+++ app.py
@@ -6,11 +6,34 @@ app = Flask(__name__)
 
 # pc 용 :
 client = MongoClient('localhost', 27017)
+db = client.CDP
 
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+#로그인 관련 라우터 끝
 @app.route('/signup_page')
 def signup_page():
     return render_template('signup_page.html')
