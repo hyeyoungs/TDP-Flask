1b00b77adaa5914206a25f1d0fa62acad0c6fab8
dlwlsrnjs <53612166+dlwlsrnjs@users.noreply.github.com>
2021-09-27 17:12:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,23 +24,24 @@ def my_page():
 def home():
     return render_template('home.html')
 
-@app.route('/til_board', methods=['POST'])
+
+@app.route('/delete', methods=['POST'])
 def delete_til():
-    til_no = request.form['til_no']
-    db.til.delete_one({'til_no': til_no})
+    til_id = request.form['til_id']
+    db.til.delete_one({'_id': til_id})
     return jsonify({'msg': '삭제 완료!'})
 
-@app.route('/til_board', methods=['GET','POST'])
-def read_til():
-    til_no = request.form['til_no']
-    temp = db.til.find_one({'til_no': til_no})
-    return jsonify({'til': temp})
-
-@app.route('/til_board', methods=['GET'])
-def all_til():
+@app.route('/til_borad', methods=['GET'])
+def show_til():
     temp = list(db.til.find({}, {'_id': False}))
-    return jsonify({'result':'success'}, {'all_til': temp})
+    return jsonify({'til_list': temp})
 
+@app.route('/', methods=['GET','POST'])
+def search_title_til():
+    til_title = request.form['search_title']      #제목으로 검색
+    # til_title = request.form['search_author'] 작가는? url이 겹칠텐데
+    temp = list(db.til.find({'title': til_title}, {'_id': False}))
+    return jsonify({'result':'success' }, {'til_list': temp})
 
 @app.route('/api/update', methods=['POST'])
 def api_update():
