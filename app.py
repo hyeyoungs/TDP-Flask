479bdbf8aed7eaf3b8888bad03d2ffcb5c24b0d2
Hyeyoung Kim <sunzero0116@gmail.com>
2021-09-27 09:57:55 +0900

    add CREATE to app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 # pc 용 :
 client = MongoClient('localhost', 27017)
+db = client.tdp
 
 @app.route('/')
 def main_page():
@@ -40,9 +41,21 @@ def api_update():
     til_title_receive = request.form['til_title_give']
     til_content_receive = request.form['til_content_give']
     current_time = datetime.now()
-    db.til.update_one({'til_id': til_id_receive}, {'til_title' : til_title_receive, 'til_content' : til_content_receive, 'til_update_day' : current_time})
+    
+    doc = {'til_id': til_id_receive}, {'til_title' : til_title_receive, 'til_content' : til_content_receive, 'til_update_day' : current_time}
+    db.til.update_one(doc)
+    return jsonify({'msg': '수정 완료!'})
 
-
+@app.route('/api/create', methods=['POST'])
+def api_create():
+    til_user_receive = request.form['user_give']
+    til_title_receive = request.form['title_give']
+    til_content_receive = request.form['content_give']
+    current_time = datetime.now()
+    
+    doc = {'til_title': til_title_receive, 'til_content': til_content_receive, 'til_user': til_user_receive, 'til_content': til_content_receive, 'til_day': current_time}
+    db.til.insert_one(doc)
+    return jsonify({'msg': 'til 작성 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
