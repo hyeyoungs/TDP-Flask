a2425ee2586a659be966d80bcbca5cdf55729096
0sunzero0 <sunzero0116@gmail.com>
2021-09-28 14:44:06 +0900

    pull test_box

--- README.md
+++ README.md
@@ -3,13 +3,22 @@
 ## TDP란
 "Today I Learned Deserter Pursuit"의 약자로서, TIL(Today I Learned) 관리 시스템입니다.
 
-## 핵심 기능
+## 1차
 - TIL CRUD
     - TIL 작성, 저장
     - TIL 수정, 삭제
 - TIL Board
-    - TIL Card List
+    - TIL public, private  
+    - list both public, private (My Page) 
+    - list only public (TIL Card List) 
+
+## 2차
 - 회원가입 CRUD
 - 로그인 / 로그아웃
-- 메인 페이지
-- 마이 페이지
+- TIL Notification
+
+## 3차
+- TIL Like
+- TIL Comment
+- TIL Rank
+- TIL Keyword
--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from flask import Flask, render_template, jsonify, request
 from datetime import datetime
 from pymongo import MongoClient
@@ -7,6 +6,7 @@ app = Flask(__name__)
 
 # pc 용 :
 client = MongoClient('localhost', 27017)
+db = client.tdp
 
 @app.route('/')
 def main_page():
@@ -20,24 +20,52 @@ def signup_page():
 def my_page():
     return render_template('my_page.html')
 
+@app.route('/create_page')
+def create_page():
+    return render_template('create.html')
+
 @app.route('/home')
 def home():
     return render_template('home.html')
-=======
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from pymongo import MongoClient
-import requests
 
-app = Flask(__name__)
+@app.route('/til_board', methods=['POST'])
+def delete_til():
+    til_id_receive = request.form['til_id_give']
+    db.tdp.delete_one({'_id': til_id_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
-client = MongoClient('mongodb://test:test@localhost', 27017)
-db = client.dbsparta_plus_week2
+@app.route('/til_board', methods=['GET', 'POST'])
+def read_til():
+    til_id_receive = request.form['til_id_give']
+    temp = db.tdp.find_one({'_id': til_id_receive})
+    return jsonify({'til': temp})
 
-@app.route('/')
-def home():
-    return render_template('index.html')
->>>>>>> Stashed changes
+@app.route('/til_board', methods=['GET'])
+def all_til():
+    temp = list(db.tdp.find({}, {'_id': False}))
+    return jsonify({'result': 'success'}, {'all_til': temp})
 
+@app.route('/api/update', methods=['POST'])
+def api_update():
+    til_id_receive = request.form['til_id_give']
+    til_title_receive = request.form['til_title_give']
+    til_content_receive = request.form['til_content_give']
+    current_time = datetime.now()
+
+    doc = {'til_title': til_title_receive, 'til_content': til_content_receive, 'til_update_day': current_time}
+    db.til.update_one({'_id': til_id_receive}, doc)
+    return jsonify({'msg': '수정 완료!'})
+
+@app.route('/api/create', methods=['POST'])
+def api_create():
+    til_user_receive = request.form['til_user_give']
+    til_title_receive = request.form['til_title_give']
+    til_content_receive = request.form['til_content_give']
+    current_time = datetime.now()
+
+    doc = {'til_title': til_title_receive, 'til_user': til_user_receive, 'til_content': til_content_receive, 'til_day': current_time}
+    db.til.insert_one(doc)
+    return jsonify({'msg': 'til 작성 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
