230245d3a048b71baf612ac911d12f79dfb5105c
0sunzero0 <sunzero0116@gmail.com>
2021-09-30 18:20:51 +0900

    add delete function, view_change function to my_page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from datetime import datetime
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -24,19 +25,19 @@ def my_page():
 def create_page():
     return render_template('create.html')
 
+@app.route('/update_page')
+def update_page():
+    return render_template('update.html')
+
 @app.route('/home')
 def home():
+    temp = list(db.til.find({}, {"_id": 0, "til_user": 1}))
+    print(temp)
     return render_template('home.html')
 
 @app.route('/til_board')
 def listing_page():
     return render_template('til_board.html')
-
-# @app.route('/til_board', methods=['POST'])
-# def delete_til():
-#     til_id_receive = request.form['til_id_give']
-#     db.til.delete_one({'_id': til_id_receive})
-#     return jsonify({'msg': '삭제 완료!'})
 
 # @app.route('/til_board', methods=['GET', 'POST'])
 # def read_til():
@@ -49,6 +50,31 @@ def all_til():
     temp = list(db.til.find({}, {'_id': False}))
     return jsonify({'result': "success", 'all_til': temp})
 
+@app.route('/api/list_myTIL', methods=['POST'])
+def read_myTIL():
+    til_user_receive = request.form['til_user_give']
+    my_til = list(db.til.find({'til_user': til_user_receive}).sort('_id', -1))
+    for doc in my_til:
+       doc["_id"] = str(doc["_id"])
+    return jsonify({'result' : 'success', 'my_til' : my_til})
+
+@app.route('/api/create', methods=['POST'])
+def api_create():
+    til_user_receive = request.form['til_user_give']
+    til_title_receive = request.form['til_title_give']
+    til_content_receive = request.form['til_content_give']
+    current_time = datetime.now()
+
+    doc = {'til_title': til_title_receive, 'til_user': til_user_receive, 'til_content': til_content_receive, 'til_day': current_time, 'til_view': True}
+    db.til.insert_one(doc)
+    return jsonify({'msg': 'til 작성 완료!'})
+
+@app.route('/api/delete', methods=['POST'])
+def api_delete():
+    til_id_receive = request.form['til_id_give']
+    db.til.delete_one({'_id' : ObjectId(til_id_receive)})
+    return jsonify({'msg': 'til 삭제 완료!'})
+
 @app.route('/api/update', methods=['POST'])
 def api_update():
     til_id_receive = request.form['til_id_give']
@@ -60,16 +86,19 @@ def api_update():
     db.til.update_one({'_id': til_id_receive}, doc)
     return jsonify({'msg': '수정 완료!'})
 
-@app.route('/api/create', methods=['POST'])
-def api_create():
-    til_user_receive = request.form['til_user_give']
-    til_title_receive = request.form['til_title_give']
-    til_content_receive = request.form['til_content_give']
-    current_time = datetime.now()
+@app.route('/api/update/view', methods=['POST'])
+def api_update_view():
+    til_id_receive = request.form['til_id_give']
+    til_view = db.til.find_one({'_id': ObjectId(til_id_receive)}, {"_id": 0, "til_view": 1})
+    view_value = til_view["til_view"]
+    if not isinstance(view_value, bool):
+        msg = '공개 여부의 값이 정확하지 않습니다.'
+    else:
+        doc = {"$set": {'til_view': not(view_value)}}
+        db.til.update_one({'_id': ObjectId(til_id_receive)}, doc)
 
-    doc = {'til_title': til_title_receive, 'til_user': til_user_receive, 'til_content': til_content_receive, 'til_day': current_time}
-    db.til.insert_one(doc)
-    return jsonify({'msg': 'til 작성 완료!'})
+    msg = '변경 완료!'
+    return jsonify({'msg': msg})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
