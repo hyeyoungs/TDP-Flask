a0584d5c9c31769e81e7e3ec10a14834be181f59
dpdtydz <dpdtydz@donga.ac.kr>
2021-10-07 15:09:57 +0900

    update_new_loginpage
    ww

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ db = client.tdp
 
 
 @app.route('/')
+def login_page():
+    return render_template('login_page.html')
+
+
+@app.route('/main_page')
 def main_page():
     return render_template('home.html')
 
