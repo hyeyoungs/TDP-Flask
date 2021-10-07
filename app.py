b7356bc4db0e5f99c8b5ddeb17298475c70de4eb
dlwlsrnjs <dlwlsrnjs1111@daum.net>
2021-10-07 14:14:29 +0900

    tdp 수정

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # pc 용 :
 client = MongoClient('localhost', 27017)
-db = client.til
+db = client.tdp
 
 
 # 위아래 두칸씩 벌려야함
@@ -46,7 +46,7 @@ def listing_page():
 @app.route('/detail')
 def detail_page():
     title = request.args.get("title")
-    content = db.til.find_one({'til_title': title}, {'_id': False})
+    content = db.tdp.find_one({'til_title': title}, {'_id': False})
     return render_template('detail.html', content=content)
 
 
@@ -59,20 +59,20 @@ def detail_page():
 @app.route('/til_board', methods=['POST'])
 def read_til():
     til_title_receive = request.form['til_title']
-    temp = list(db.til.find({'til_title': til_title_receive}, {'_id': False}))
+    temp = list(db.tdp.find({'til_title': til_title_receive}, {'_id': False}))
     return jsonify({'til': temp})
 
 
 @app.route('/til_board_listing', methods=['GET'])
 def all_til():
-    temp = list(db.til.find({}, {'_id': False}))
+    temp = list(db.tdp.find({}, {'_id': False}))
     return jsonify({'result': "success", 'all_til': temp})
 
 
 @app.route('/api/list_myTIL', methods=['POST'])
 def read_mytil(): # pep8에러 함수이름
     til_user_receive = request.form['til_user_give']
-    my_til = list(db.til.find({'til_user': til_user_receive}).sort('_id', -1))
+    my_til = list(db.tdp.find({'til_user': til_user_receive}).sort('_id', -1))
     for doc in my_til:
         doc["_id"] = str(doc["_id"])
     return jsonify({'result': 'success', 'my_til': my_til})
@@ -80,13 +80,13 @@ def read_mytil(): # pep8에러 함수이름
 
 @app.route('/home_listing', methods=['GET'])
 def home_til():
-    temp = list(db.til.find({}, {'_id': False}).sort("_id", -1))
+    temp = list(db.tdp.find({}, {'_id': False}).sort("_id", -1))
     return jsonify({'result': "success", 'home_til': temp})
 
 
 @app.route('/home_ranking', methods=['GET'])
 def home_ranking():
-    agg_result = list(db.til.aggregate([
+    agg_result = list(db.tdp.aggregate([
         {"$group":
             {
                 "_id": "$til_user",
@@ -109,14 +109,14 @@ def api_create():
 
     doc = {'til_title': til_title_receive, 'til_user': til_user_receive, 'til_content': til_content_receive,
            'til_day': current_time, 'til_view': True}
-    db.til.insert_one(doc)
+    db.tdp.insert_one(doc)
     return jsonify({'msg': 'til 작성 완료!'})
 
 
 @app.route('/api/delete', methods=['POST'])
 def api_delete():
     til_id_receive = request.form['til_id_give']
-    db.til.delete_one({'_id': ObjectId(til_id_receive)})
+    db.tdp.delete_one({'_id': ObjectId(til_id_receive)})
     return jsonify({'msg': 'til 삭제 완료!'})
 
 
@@ -128,20 +128,20 @@ def api_update():
     current_time = datetime.now()
 
     doc = {"$set": {'til_title': til_title_receive, 'til_content': til_content_receive, 'til_update_day': current_time}}
-    db.til.update_one({'_id': ObjectId(til_id_receive)}, doc)
+    db.tdp.update_one({'_id': ObjectId(til_id_receive)}, doc)
     return jsonify({'msg': '수정 완료!'})
 
 
 @app.route('/api/update/view', methods=['POST'])
 def api_update_view():
     til_id_receive = request.form['til_id_give']
-    til_view = db.til.find_one({'_id': ObjectId(til_id_receive)}, {"_id": 0, "til_view": 1})
+    til_view = db.tdp.find_one({'_id': ObjectId(til_id_receive)}, {"_id": 0, "til_view": 1})
     view_value = til_view["til_view"]
     if not isinstance(view_value, bool):
         msg = '공개 여부의 값이 정확하지 않습니다.'
     else:
         doc = {"$set": {'til_view': not (view_value)}}
-        db.til.update_one({'_id': ObjectId(til_id_receive)}, doc)
+        db.tdp.update_one({'_id': ObjectId(til_id_receive)}, doc)
 
     msg = '변경 완료!'
     return jsonify({'msg': msg})
